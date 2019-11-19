42acbd5e170500be1f45f375661d0824ff41bf5d
aironman <alonsoir@gmail.com>
2019-11-19 16:42:22 +0100

    adding useful confluent commands and other stuff

--- startup-when-confluent-is-installed.sh
+++ startup-when-confluent-is-installed.sh
@@ -1,4 +1,6 @@
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+# actual version requires jdk8
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home 
+# (/usr/libexec/java_home -v 1.8)
 set -x CONFLUENT_CLI_HOME /Users/aironman/confluent-5.3.1/confluent-cli
 set -x CONFLUENT_HOME /Users/aironman/confluent-5.3.1
 set PATH $CONFLUENT_CLI_HOME/bin $PATH
@@ -6,4 +8,4 @@ set PATH $CONFLUENT_HOME/bin $PATH
 set PATH $JAVA_HOME $PATH
 # start the platform
 confluent local start
-
+# http://localhost:9021/clusters
